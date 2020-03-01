b064adcbb3fb2e41607c540175f443557f445cab
chrisrm86 <christianrmoran86@gmail.com>
2020-03-01 14:58:59 -0300

    look source code of page as optional

--- Ping_checker.py
+++ Ping_checker.py
@@ -26,14 +26,24 @@ def check_internet_connection():
    print("\n")
    print("Headers: " + str(webUrl.headers))
    print("---------------------------------")
-   print("Source code from " + webUrl.url)
-   print("\n")
 
-   for lines in webUrl.readlines():
-       print(lines)
-       print('\n')
+   def look_source_code(webUrl):
+      user_response = str(input("Look source code from {}? y/n: ".format(webUrl.url)))
+      print('\n')
+      if user_response == 'Y' or user_response == 'y':
+         print('Source code from {} :'.format(webUrl.url))
+         print('\n')
+         for lines in webUrl.readlines():
+            print(lines)
+            print('\n')
+      elif user_response == 'N' or user_response == 'n':
+         pass
+      else:
+         return look_source_code(webUrl)
+
+   look_source_code(webUrl)
 
 check_internet_connection()
-
 print("Press Enter/Intro to exit")
+
 input()
